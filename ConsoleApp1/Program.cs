0b6f8237aa2e3dddabd27f0bcb0ac49882ffb782
b10956054 <a092819984@gmail.com>
2022-03-08 17:46:25 +0800

    123

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,12 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double z;
+            double meal = 100;
+            double tip_percent = 0.15;
+            double tax_percent = 0.08;
+            z = meal * tip_percent + meal * tax_percent + meal;
+            Console.Write(z);
         }
     }
 }
